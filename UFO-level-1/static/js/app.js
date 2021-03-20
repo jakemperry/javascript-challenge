c578292e95e0287ac8a448afb393d23db020cc80
jakemperry <Jakemperry@gmail.com>
2021-03-19 19:19:32 -0700

    add filter, button variables, add events, start event handler function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the filter id
+var filter = d3.select("#filters")
+// Select the button id
+var button = d3.select("#filter-btn")
+
+// Creat event handlers
+filter.on("submit", runSubmit);
+form.on("click", runSubmit);
+
+// Event handler function
+function runSubmit(){
+    
+}
