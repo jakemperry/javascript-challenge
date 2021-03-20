7b97a3fd68350035a4438f27fdd3ddc52789524e
jakemperry <Jakemperry@gmail.com>
2021-03-19 19:32:59 -0700

    add filter functionality within the event handler function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,9 +8,21 @@ var button = d3.select("#filter-btn")
 
 // Creat event handlers
 filter.on("submit", runSubmit);
-form.on("click", runSubmit);
+button.on("click", runSubmit);
 
 // Event handler function
 function runSubmit(){
-    
+    // Prevent page refresh
+    d3.event.preventDefault();
+
+    var filterElement = d3.select("#datetime");
+
+    var inputDate = filterElement.property("value");
+
+    console.log(inputDate);
+    console.log(tableData);
+
+    var filteredTable = tableData.filter(tableData => tableData.datetime === inputDate);
+
+    console.log(filteredTable);
 }
